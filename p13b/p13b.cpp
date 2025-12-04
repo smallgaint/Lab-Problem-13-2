97148e3d7ecdb18f179a849871e20b444a91cdc5
s-hong.1 <s-hong.1@onu.edu>
2025-12-04 12:54:35 -0500

    Completed P13B-2

--- p13b/p13b.cpp
+++ p13b/p13b.cpp
@@ -3,16 +3,61 @@
 using namespace std;
 
 bool isLeapYear(int year);
+int daysInMonth(int month, int year);
 
 int main() {
+	int monthInput;
 	int year;
+	string month;
 
 	while (true) {
-		cout << "Enter a year or Q to quit: ";
-		cin >> year;
+		cout << "Enter a month and year or Q to quit: ";
+		cin >> monthInput >> year;
 
 		if (cin.fail()) {
 			return 0;
+		}
+
+		switch (monthInput) {
+			case 1:
+				month = "January";
+				break;
+			case 2:
+				month = "February";
+				break;
+			case 3:
+				month = "March";
+				break;
+			case 4:
+				month = "April";
+				break;
+			case 5:
+				month = "May";
+				break;
+			case 6:
+				month = "June";
+				break;
+			case 7:
+				month = "July";
+				break;
+			case 8:
+				month = "August";
+				break;
+			case 9:
+				month = "September";
+				break;
+			case 10:
+				month = "October";
+				break;
+			case 11:
+				month = "November";
+				break;
+			case 12:
+				month = "December";
+				break;
+			default:
+				cout << "Invalid month." << endl;
+				continue;
 		}
 
 		if (year < 1582) {
@@ -20,12 +65,7 @@ int main() {
 			continue;
 		}
 
-		if (isLeapYear(year)) {
-			cout << year << " is a leap year." << endl;
-		}
-		else {
-			cout << year << " is not a leap year." << endl;
-		}
+		cout << month << " " << year << " has " << daysInMonth(monthInput, year) << " days." << endl;
 	}
 
 
@@ -43,5 +83,17 @@ bool isLeapYear(int year) {
 	}
 	else {
 		return true;
+	}
+}
+
+int daysInMonth(int month, int year) {
+	if (month == 2) {
+		return isLeapYear(year) ? 29 : 28;
+	}
+	else if (month == 4 || month == 6 || month == 9 || month == 11) {
+		return 30;
+	}
+	else {
+		return 31;
 	}
 }
